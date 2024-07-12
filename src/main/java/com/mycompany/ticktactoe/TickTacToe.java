0d3c37d4377337b00f8e86e4cd10f70f39495e7f
Ashley Pillay <ashleyjulianpillay@gmail.com>
2024-07-12 03:30:39 +0200

    File structure

---
+++ src/main/java/com/mycompany/ticktactoe/TickTacToe.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ticktactoe;
+
+/**
+ *
+ * @author ashley
+ */
+public class TickTacToe {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
